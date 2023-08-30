3bc453a56cc047d255c3ade5b77fb25513b86439
Jörn-Michael Miehe <40151420+ldericher@users.noreply.github.com>
2023-08-30 23:49:07 +0000

    preliminary metrics "CPU" and "DISK"

--- kiwi_simple_metrics/main.py
+++ kiwi_simple_metrics/main.py
@@ -1,11 +1,38 @@
+import os
+import shutil
+
 import psutil
 
 from .settings import SETTINGS
 
 
 def main() -> None:
-    print(psutil.cpu_percent(interval=1))
+    # env parameters
     print(SETTINGS.model_dump())
+
+    # CPU metric
+    print(psutil.cpu_percent(interval=1))
+
+    # MEM metric
+
+    # DISK metric
+    # TODO test this using timeit
+    for path in SETTINGS.disk.params:
+        try:
+            sv = os.statvfs(path)
+            percent = sv.f_bavail / sv.f_blocks * 100
+        except ZeroDivisionError:
+            percent = 0
+
+        print(f"{path} Free (sv): {percent:.2f} %")
+
+        try:
+            total, _, free = shutil.disk_usage(path)
+            percent = free / total * 100
+        except ZeroDivisionError:
+            percent = 0
+
+        print(f"{path} Free (du): {percent:.2f} %")
 
 
 if __name__ == "__main__":
